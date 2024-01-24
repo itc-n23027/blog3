ed6cbf7db4541e8e8554b478bfac335232e85541
itc-n23027 <n23027@std.it-college.ac.jp>
2024-01-24 07:17:33 +0000

    3.5 hero.jsのテキストのスタイルを設定する

--- components/hero.js
+++ components/hero.js
@@ -1,9 +1,15 @@
-export default function Hero ({ title, subtitle, image0n = false }) {
+import styles from 'styles/hero.module.css'
+
+const Hero = ({ title, subtitle, imageOn = false }) => {
   return (
     <div>
-      <h1>{title}</h1>
-      <p>{subtitle}</p>
-      {image0n && <figure> [画像] </figure>}
+      <div className={styles.text}>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.subtitle}>{subtitle}</p>
+      </div>
+      {imageOn && <figure> [画像] </figure>}
     </div>
   )
 }
+
+export default Hero
