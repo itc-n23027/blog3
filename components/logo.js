d83988099b2de408fcdfed87d4bd02b49298bb29
itc-n23027 <n23027@std.it-college.ac.jp>
2024-01-24 07:30:28 +0000

    3.6 styles/logo.module.cssを追加

--- components/logo.js
+++ components/logo.js
@@ -1,7 +1,9 @@
 import Link from 'next/link'
-
-const Logo = () => {
-  return <Link href='/'>CUBE</Link>
+import styles from 'styles/logo.module.css'
+export default function Logo ({ boxOn = false }) {
+  return (
+    <Link href='/' className={boxOn ? styles.box : styles.basic}>
+      CUBE
+    </Link>
+  )
 }
-
-export default Logo
