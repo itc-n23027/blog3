c6cf10fc929d04deded1fd245d5b3705fb1ef1fb
itc-n23027 <n23027@std.it-college.ac.jp>
2024-02-06 07:33:22 +0000

    9.1 eyecatch画像追加。blog/scheduleにmicroページを表示

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -12,6 +12,9 @@ import {
 import ConvertBody from 'components/convert-body'
 import PostCategories from 'components/post-categories'
 import Image from 'next/image'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
+import { getImageBuffer } from 'lib/getImageBuffer'
 
 export default function Schedule ({
   title,
@@ -60,18 +63,24 @@ export default function Schedule ({
 }
 
 export async function getStaticProps () {
-  const slug = 'schedule'
+  const slug = 'micro'
 
   const post = await getPostBySlug(slug)
 
   const description = extractText(post.content)
+
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+
+  const imageBuffer = await getImageBuffer(eyecatch.url)
+  const { base64 } = await getPlaiceholder(imageBuffer)
+  eyecatch.blurDataURL = base64
 
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
-      eyecatch: post.eyecatch,
+      eyecatch: eyecatch,
       categories: post.categories,
       description: description
     }
